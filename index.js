c972807b9a5eef2b707f3e2f80a03687f7d2fd27
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-12 11:13:41 +0600

    add html and css and javascript for the project

---
+++ index.js
@@ -0,0 +1,8 @@
+function add(){
+    var myVar = document.querySelector("#para");
+    myVar.classList.add( "para_cls" );
+} 
+function removed(){
+    var myVar = document.querySelector("#para");
+    myVar.classList.remove("para_cls");
+}
